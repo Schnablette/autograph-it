935a24c80e353409900c4222aa5ce19a09032a71
Ann Schnabel <anntschnabel@gmail.com>
2022-12-19 23:25:17 -0500

    preliminary setup

---
+++ components/Nav.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../assets/Logo.png';
+
+const Nav = () => {
+  return (
+    <nav className="pb-12 flex justify-between min-w-full">
+      <Link href="/">
+        <Image src={logo} alt="Logo for Autograph It" className="max-w-xs" />
+      </Link>
+
+      <div>
+        <button className="bg-fern-500 hover:bg-fern-200 active:bg-fern-300 text-ebony-800 font-bold py-2 px-4 rounded">
+          Join Beta
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export { Nav };
