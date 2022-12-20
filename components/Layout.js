012619fd3810739e25d76a14f9c1d1f24fd7b168
Ann Schnabel <anntschnabel@gmail.com>
2022-12-19 23:56:56 -0500

    basic site

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,10 @@
 export const Layout = ({ children }) => {
-  return <div className="p-10 overflow-hidden">{children}</div>;
+  return (
+    <>
+      <div className="bg-fern-500"><p className="text-ebony-800 text-sm text-center py-2">
+        Your NFT is about to get personal - Q1 2023
+      </p></div>
+      <div className="px-10 pt-10 overflow-hidden">{children}</div>
+    </>
+  );
 };
--- components/Nav.js
+++ components/Nav.js
@@ -4,17 +4,23 @@ import logo from '../assets/Logo.png';
 
 const Nav = () => {
   return (
-    <nav className="pb-12 flex justify-between min-w-full">
-      <Link href="/">
-        <Image src={logo} alt="Logo for Autograph It" className="max-w-xs" />
-      </Link>
-
-      <div>
-        <button className="bg-fern-500 hover:bg-fern-200 active:bg-fern-300 text-ebony-800 font-bold py-2 px-4 rounded">
-          Join Beta
-        </button>
-      </div>
-    </nav>
+    <>
+      <nav className="pb-12 flex justify-between min-w-full">
+        <Link href="/">
+          <Image
+            width="260"
+            height="36"
+            src={logo}
+            alt="Logo for Autograph It"
+          />
+        </Link>
+        <div>
+          <button className="bg-fern-500 hover:bg-fern-200 active:bg-fern-300 text-ebony-800 font-bold py-2 px-4 rounded">
+            Join Beta
+          </button>
+        </div>
+      </nav>
+    </>
   );
 };
 
